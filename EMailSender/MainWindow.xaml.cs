00d47f23bea2c3332f20d71e51edc2ef7c390b7e
Usov Alexandr <ussov39@gmail.com>
2020-09-29 13:01:12 +0300

    Добавьте файлы проекта.

---
+++ EMailSender/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using EMailSenderLib;
+
+
+namespace EMailSender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            var sendMail = new MailSender();
+            ResultLbl.Content =  sendMail.Send(UserNameBox.Text, passwordBox.Password, SubjectBox.Text, BodyBox.Text, ReciverBox.Text);
+        }
+    }
+}
---
+++ EMailSenderLib/MailSender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace EMailSenderLib
+{
+    public class MailSender
+    {
+        public string Send(string user, string password, string subject, string body, string reciver)
+        {
+            string from = user;
+            string to = reciver;
+
+            MailMessage mm = new MailMessage(from, to);
+
+
+            mm.Subject = subject;
+            mm.Body = body;
+            mm.IsBodyHtml = false;
+
+            SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25);
+            sc.EnableSsl = true;
+            sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+            sc.UseDefaultCredentials = false;
+            sc.Credentials = new NetworkCredential(user, password);
+            
+
+            try
+            {
+
+                sc.Send(mm);
+                return "Письмо отправленно";
+            }
+            catch(Exception ex)
+            {
+                return "Не удалось отправить письмо" + ex.ToString();
+            }
+
+        }
+
+    }
+}
